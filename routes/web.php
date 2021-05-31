7f46bda3eb3fd9a514858251b5b0cdbff0463196
Abdul Samad <abdul.samad@click.aero>
2021-05-31 19:57:49 +0400

    Setup the application routes

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,35 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+$router->group(['prefix' => 'api/auth'], function ($router) {
+    $router->post('login', 'AuthController@login');
+    $router->post('logout', 'AuthController@logout');
+    $router->post('refresh', 'AuthController@refresh');
+    $router->post('me', 'AuthController@me');
+    $router->get('users', 'AuthController@index');
+});
+
+$router->group(['middleware' => 'jwt.auth',    'prefix' => 'api'], function ($router) {
+
+    /**
+     * Routes for resource post
+     */
+    $router->get('posts', 'PostsController@all');
+    $router->get('posts/{id}', 'PostsController@get');
+    $router->get('posts/{id}/comments', 'PostsController@comments');
+    $router->post('posts', 'PostsController@add');
+    $router->put('posts/{id}', 'PostsController@put');
+    $router->delete('posts/{id}', 'PostsController@remove');
+
+    /**
+     * Routes for resource comment
+     */
+    $router->get('comments', 'CommentsController@all');
+    $router->get('comments/{id}', 'CommentsController@get');
+    $router->post('comments', 'CommentsController@add');
+    $router->put('comments/{id}', 'CommentsController@put');
+    $router->delete('comments/{id}', 'CommentsController@remove');
+});
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
